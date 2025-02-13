c34af525254bb6c7ed1172db55f4cceb3a11c820
OlympiaGYM-UCE <ogym703@gmail.com>
2025-02-13 02:46:47 -0500

    Update login.jsx

--- src/login.jsx
+++ src/login.jsx
@@ -3,7 +3,7 @@
 function Login() {
   return (
     <div>
-      <h1>Página de Iniciar sesión</h1>
+      <h1>Páginaaa de Iniciar sesión</h1>
       {/* Aquí puedes agregar más contenido para tu página de login */}
     </div>
   );
